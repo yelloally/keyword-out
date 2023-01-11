09aae1de124fd67e3cdbd7ff0aa56f0cebf19423
Alona <122108963+yelloally@users.noreply.github.com>
2023-01-11 19:06:49 +0100

    Add files via upload

---
+++ ref_c_sharp/Program.cs
@@ -0,0 +1,15 @@
+﻿//we create a method where made a condition for ref
+void PracticeWithRef(ref int num)
+{
+    //ref will add 1 to the original number
+    num++;
+    Console.WriteLine($"Number in method: {num}");
+}
+
+//our original number 'before' using ref
+int beforeAfter = 7;
+Console.WriteLine($"Number before method: {beforeAfter}");
+
+//our original number 'after' using ref
+PracticeWithRef(ref beforeAfter);
+Console.WriteLine($"Number after method: {beforeAfter}");
